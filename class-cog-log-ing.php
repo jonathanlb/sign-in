40fc9479282d9a8b926cba222308678bbb8ae18c
Jonathan Bredin <bredin@acm.org>
2025-09-20 15:39:23 -0500

    initial prototype

---
+++ class-cog-log-ing.php
@@ -0,0 +1,342 @@
+<?php
+/**
+ * cog-log-ing install, init, admin
+ *
+ * @package    cog-log-ing
+ * @author     Jonathan Bredin <bredin@acm.org>
+ * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link       https://github.com/jonathanlb
+ * @since      0.0.1
+ */
+
+/**
+ * cog-log-ing
+ */
+class Cog_Log_Ing {
+	static $aws_config = null;
+	static $cognito_client = null;
+	static $initialized = false;
+    static $version = '0.0.1';
+
+    /**
+     * Initialize the plugin
+     *
+     * @since 0.0.1
+     */
+    public static function init() {
+        error_log( 'Cog_Log_Ing::init' );
+        if ( self::$initialized ) {
+			return;
+		}
+        error_log( 'Cog_Log_Ing::init registering actions' );
+
+        self::$initialized = true;
+        add_action( 'admin_init', array( 'Cog_Log_Ing', 'admin_init' ) );
+		add_action( 'admin_menu', array( 'Cog_Log_Ing', 'admin_menu' ) );
+
+		global $wp;
+		$url_vars = array( 'password', 'token', 'user_name' );
+		foreach ($url_vars as $var) {
+			$wp->add_query_var( $var );
+		}
+
+		add_filter( 'the_content', array( 'Cog_Log_Ing', 'filter_protected_content' ) );
+    }
+
+    /**
+     * Respond to admin_init action.
+     */
+    public static function admin_init() {
+        function aws_settings_callback() {
+			esc_html_e( 'AWS Settings' );
+		}
+
+		/**
+		 * Retrieve an option value and populate it to input field.
+		 *
+		 * @param array $text_input Text-input attributes.
+		 */
+		function text_input_callback( $text_input ) {
+			$option_group = $text_input['option_group'];
+			$option_id    = $text_input['option_id'];
+			$option_name  = "{$option_group}[{$option_id}]";
+			$options      = get_option( $option_group );
+			$option_value = $options[ $option_id ] ?? '';
+			?>
+			<input type="text" size="32" id="<?php echo esc_attr( $option_id ); ?>"
+		name="<?php echo esc_attr( $option_name ); ?>"
+		value="<?php echo esc_attr( $option_value ); ?>" />
+			<?php
+		}
+
+		add_settings_section(
+			'aws_settings_section',
+			__( 'AWS Settings' ),
+			'aws_settings_callback',
+			'cog_log_ing_settings'
+		);
+		add_settings_field(
+			'aws_credentials_path',
+			__( 'Path to credentials file' ),
+			'text_input_callback',
+			'cog_log_ing_settings',
+			'aws_settings_section',
+			array(
+				'label_for'    => 'aws_credentials_path',
+				'option_group' => 'cog_log_ing_settings',
+				'option_id'    => 'aws_credentials_path',
+			)
+		);
+		add_settings_field(
+			'aws_credentials_profile',
+			__( 'Credentials profile' ),
+			'text_input_callback',
+			'cog_log_ing_settings',
+			'aws_settings_section',
+			array(
+				'label_for'    => 'aws_credentials_profile',
+				'option_group' => 'cog_log_ing_settings',
+				'option_id'    => 'aws_credentials_profile',
+			)
+		);
+		add_settings_field(
+			'link_timeout',
+			__( 'Link Timeout' ),
+			'text_input_callback',
+			'cog_log_ing_settings',
+			'aws_settings_section',
+			array(
+				'label_for'    => 'link_timeout',
+				'option_group' => 'cog_log_ing_settings',
+				'option_id'    => 'link_timeout',
+			)
+		);
+		add_settings_field(
+			'aws_region',
+			__( 'Region' ),
+			'text_input_callback',
+			'cog_log_ing_settings',
+			'aws_settings_section',
+			array(
+				'label_for'    => 'aws_region',
+				'option_group' => 'cog_log_ing_settings',
+				'option_id'    => 'aws_region',
+			)
+		);
+		add_settings_field(
+			'aws_version',
+			__( 'Version' ),
+			'text_input_callback',
+			'cog_log_ing_settings',
+			'aws_settings_section',
+			array(
+				'label_for'    => 'aws_version',
+				'option_group' => 'cog_log_ing_settings',
+				'option_id'    => 'aws_version',
+			)
+		);
+
+		register_setting( 'cog_log_ing_settings', 'cog_log_ing_settings' );
+    }
+
+    /**
+     * Respond to admin_menu action.
+     */
+    public static function admin_menu() {
+        add_submenu_page(
+            'options-general.php',
+            'cog-log-ing Settings',
+            'cog-log-ing',
+            'manage_options',
+            'cog-log-ing',
+            array( 'Cog_Log_Ing', 'render_settings_page' )
+        );
+    }
+
+    /**
+	 * Build an AWS SDK object from plugin options.
+	 *
+	 * @param array $aws_opts configuration to override global options.
+	 */
+	private static function create_aws_sdk( $aws_opts ) {
+		$options    = get_option( 'cog_log_ing_settings' );
+		$aws_config = array(
+			'region'  => isset( $aws_opts['region'] ) ? $aws_opts['region'] : $options['aws_region'],
+			'version' => $options['aws_version'],
+		);
+
+		$profile          = $options['aws_credentials_profile'];
+		$credentials_path = $options['aws_credentials_path'];
+
+		if ( $credentials_path ) {
+			if ( ! str_starts_with( $credentials_path, '/' ) ) {
+				$credentials_path = COG_LOG_ING__PLUGIN_DIR . $credentials_path;
+			}
+			$provider = CredentialProvider::ini( $profile, $credentials_path );
+			$provider = CredentialProvider::memoize( $provider );
+
+			$aws_config['credentials'] = $provider;
+		} else {
+			$aws_config['profile'] = $profile;
+		}
+
+		$sdk = new Aws\Sdk( $aws_config );
+		return $sdk;
+	}
+
+   	/**
+	 * Provide a cognito client.
+	 *
+	 * @param array $aws_opts configuration to override global options.
+	 */
+	public static function cognito_client( $aws_opts ) {
+		if ( $aws_opts == self::$aws_config && self::$cognito_client != null ) {
+			return self::$cognito_client;
+		}
+
+		self::$aws_config = $aws_opts;
+
+		$aws_sdk         = self::create_aws_sdk( $aws_opts );
+		self::$cognito_client = $aws_sdk->createCognitoIdentityProvider();
+		return self::$cognito_client;
+	}
+
+	/**
+	 * Remove Filter protected content.
+	 * 
+	 */
+	public static function filter_protected_content( $content ) {
+		$start_pos = strpos( $content, '[cogloging_require_auth' );
+		if ( ! $start_pos ) {
+			return $content;
+		}
+
+		// Check if token is in URL and valid
+		// TODO: validate token
+		$token = get_query_var( 'token', null );
+		if ( $token == 'xyz' ) {
+			$pattern = '/\[cogloging_require_auth[^\]]*\]/s';
+			return preg_replace( $pattern, '', $content );
+		} else if ( $token !== null ) {
+            wp_redirect( get_permalink( get_the_ID() ) );
+			return;
+		}
+
+        // Check if user_name and password are in URL and valid
+        // If so, generate token and redirect
+        $user_name = get_query_var( 'user_name', null );
+        $password = get_query_var( 'password', null );
+		// TODO: validate user_name and password
+        if ( $user_name == "jonathan" && $password == "secret" ) {
+            $result = get_permalink( get_the_ID() );
+            wp_redirect( $result . "?token=xyz" );
+			return;
+        }
+
+		$login_msg = 'Please log in';
+		if ( $user_name || $password ) {
+			$login_msg = 'Invalid login';
+		}
+		$result = substr($content, 0, $start_pos) . self::render_login( $login_msg );
+
+		return $result;
+	}
+
+   /**
+	 * Respond to plugin_activation events by writing default settings.
+	 */
+	public static function plugin_activation() {
+		$options = get_option( 'cog_log_ing_settings' );
+		if ( ! isset( $options['aws_credentials_path'] ) ) {
+			$options['aws_credentials_path'] = '';
+		}
+		if ( ! isset( $options['aws_credentials_profile'] ) ) {
+			$options['aws_credentials_profile'] = 'default';
+		}
+		if ( ! isset( $options['aws_region'] ) ) {
+			$options['aws_region'] = 'us-east-2';
+		}
+		if ( ! isset( $options['aws_version'] ) ) {
+			$options['aws_version'] = 'latest';
+		}
+		if ( ! isset( $options['link_timeout'] ) ) {
+			$options['link_timeout'] = '+8 hours';
+		}
+
+		add_option( 'cog_log_ing_settings', $options );
+	}
+
+	/**
+	 * Respond to plugin_deactivation events.
+	 */
+	public static function plugin_deactivation() {
+		delete_option( 'cog_log_ing_settings' );
+	}
+
+	/**
+	 * Display HTML login form that will redirect to the same page with filled in 
+	 * user_name and password parameters.
+	 * 
+	 * @param string $error_msg message to display above the form.
+	 * 
+	 * TODO: load css style
+	 */
+    static function render_login( $error_msg = '' ) {
+        ob_start();
+        ?>
+<link rel="stylesheet" href="<?php echo plugin_dir_url( __FILE__ ) . 'style.css'; ?>" />
+<form class="cog-log-ing-login"
+    action='<?php echo get_permalink( get_the_ID() ); ?>'>
+    <div class="cog-log-ing-error"><?php echo esc_html( $error_msg ); ?></div>
+    <div class="cog-log-ing-label-input-pair">
+        <label for="user_name">Email:</label>
+        <input
+            type="text"
+            id="user_name"
+            name="user_name"
+            value=""
+            autocomplete="username"
+        />
+    </div>
+    <div class="cog-log-ing-label-input-pair">
+        <label for="password">Password:</label>
+        <input
+            type="password"
+            id="password"
+            name="password"
+            value=""
+            autocomplete="current-password"
+        />
+    </div>
+    <div class="cog-log-ing-label-input-pair">
+        <input type="submit" value="Log In" />
+    </div>
+</form>
+        <?php
+        return ob_get_clean();
+    }
+
+   	/**
+	 * Build the plugin settings page.
+	 */
+    static function render_settings_page() {
+        if ( ! current_user_can( 'manage_options' ) ) {
+            return;
+        }
+
+        		?>
+<div class="wrap">
+	<h2><?php esc_html_e( 'Cog Log Ing Settings' ); ?></h2>
+
+	<form method="post" action="options.php">
+		<?php
+			settings_fields( 'cog_log_ing_settings' );
+			do_settings_sections( 'cog_log_ing_settings' );
+			submit_button();
+		?>
+	</form>
+
+</div><!-- /.wrap -->
+		<?php
+    }
+}
---
+++ cog-log-ing.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package cog-log-ing
+ * @version 0.0.1
+ */
+
+/*
+Plugin Name: cog-log-ing
+Plugin URI: https://github.com/jonathanlb
+Description: Provide authenticated access to WP content.
+Version: 0.0.1
+Author: Jonathan Bredin
+Author URI: https://bredin.org
+License: https://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+*/
+
+// Make sure we don't expose any info if called directly.
+if ( ! function_exists( 'add_action' ) ) {
+	echo 'Do not call plugins directly.';
+	exit;
+}
+
+require 'vendor/autoload.php';
+define( 'COG_LOG_ING__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+register_activation_hook( __FILE__, array( 'Cog_Log_Ing', 'plugin_activation' ) );
+register_deactivation_hook( __FILE__, array( 'Cog_Log_Ing', 'plugin_deactivation' ) );
+
+require_once COG_LOG_ING__PLUGIN_DIR . 'class-cog-log-ing.php';
+
+add_action( 'init', array( 'Cog_Log_Ing', 'init' ) );
